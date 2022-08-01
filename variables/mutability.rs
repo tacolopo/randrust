dd7deafcf0d6d4b6536d7af9c1530cbbc9386d05
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-07-31 20:31:19 -0400

    Update mutability.rs

--- variables/mutability.rs
+++ variables/mutability.rs
@@ -1,6 +1,13 @@
 fn main() {
     let x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    println!("The value of x is {x}");
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("Inner x = {x}");
+    }
+    println!("outer x = {x}")
 }
+//you aren’t allowed to use mut with constants. Constants aren’t just 
+//immutable by default—they’re always immutable.
+//    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
