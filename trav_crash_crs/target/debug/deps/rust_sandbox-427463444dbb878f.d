5e7c80b4753cb55389f0ff9aa4ed5966dc0e657a
tacolopo <ciryl@tutanota.com>
2022-12-13 09:10:49 -0500

    add calculator

---
+++ trav_crash_crs/target/debug/deps/rust_sandbox-427463444dbb878f.d
@@ -0,0 +1,6 @@
+/Users/makand/Documents/randrust/trav_crash_crs/target/debug/deps/rust_sandbox-427463444dbb878f.rmeta: src/main.rs src/cli.rs
+
+/Users/makand/Documents/randrust/trav_crash_crs/target/debug/deps/rust_sandbox-427463444dbb878f.d: src/main.rs src/cli.rs
+
+src/main.rs:
+src/cli.rs:
---
+++ trav_crash_crs/target/debug/deps/rust_sandbox-4744b86b90820348.d
@@ -0,0 +1,6 @@
+/Users/makand/Documents/randrust/trav_crash_crs/target/debug/deps/rust_sandbox-4744b86b90820348.rmeta: src/main.rs src/cli.rs
+
+/Users/makand/Documents/randrust/trav_crash_crs/target/debug/deps/rust_sandbox-4744b86b90820348.d: src/main.rs src/cli.rs
+
+src/main.rs:
+src/cli.rs:
